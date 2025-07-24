0d2171ae522d1684b5558d2e7670ca8fa22ca51d
Phalmora <info@phalmora.com>
2025-07-24 19:11:59 +0530

    Setup done for deployment

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ await connectDB();
 await connectCloudinary();
 
 //Allowed multiple origins 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://greencart-orcin.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
